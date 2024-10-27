d380be1ee90b3b4a2bf601af7b090444cf4b4c05
Peter Ebert <36245614+ptrebert@users.noreply.github.com>
2024-10-27 12:47:37 +0100

    change set_qual_filter to also set REF for missing

--- workflow/scripts/set_qual_filter.py
+++ workflow/scripts/set_qual_filter.py
@@ -12,6 +12,17 @@ def main():
         columns = line.strip().split()
         columns[5] = str(60)
         columns[6] = "PASS"
+
+        # temp fix - ?
+        # SHAPEIT does not allow missing genotypes in ref panel
+        # - can't drop all variants that have a missing GT in one
+        # sample (= essentially all?), so setting everything missing
+        # to REF. Using bcftools plugin setGT does not work because
+        # it replaces a partial missing like ".|1" with "0|0" for
+        # whatever reason ...
+
+        columns = columns[:9] + [gt.replace(".", "0") for gt in columns[9:]]
+
         out_line = "\t".join(columns)
         sys.stdout.write(out_line + "\n")
 
